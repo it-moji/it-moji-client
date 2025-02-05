9681d41e02721d897b4edbb68fe7cb0a32a91624
ChanLee_KR <cl700120@gmail.com>
2025-02-05 20:23:00 +0900

    fix: disabled 상태에서 a 태그 클릭 되는 현상 수정

--- src/widgets/announcement-list/ui/page-controller.tsx
+++ src/widgets/announcement-list/ui/page-controller.tsx
@@ -13,6 +13,9 @@ export interface PageControllerProps {
 
 export const PageController: React.FC<PageControllerProps> = ({ page, total, baseURL }) => {
   const href = createSearchParamsToURL(baseURL)
+  const handleClick: React.MouseEventHandler<HTMLAnchorElement> = (e) => {
+    if (e.currentTarget.dataset.disabled) e.preventDefault()
+  }
 
   return (
     <Pagination.Root
@@ -20,20 +23,27 @@ export const PageController: React.FC<PageControllerProps> = ({ page, total, bas
       getItemProps={(page) => ({
         component: Link,
         href: href([GetPostListParamsSchema.Enum.page, page]),
+        onClick: handleClick,
       })}
     >
       <Group gap={8}>
-        <Pagination.First component={Link} href={baseURL} />
+        <Pagination.First component={Link} href={baseURL} onClick={handleClick} />
         <Pagination.Previous
           component={Link}
           href={href([GetPostListParamsSchema.Enum.page, page - 1])}
+          onClick={handleClick}
         />
         <Pagination.Items />
         <Pagination.Next
           component={Link}
           href={href([GetPostListParamsSchema.Enum.page, page + 1])}
+          onClick={handleClick}
         />
-        <Pagination.Last component={Link} href={href([GetPostListParamsSchema.Enum.page, total])} />
+        <Pagination.Last
+          component={Link}
+          href={href([GetPostListParamsSchema.Enum.page, total])}
+          onClick={handleClick}
+        />
       </Group>
     </Pagination.Root>
   )
