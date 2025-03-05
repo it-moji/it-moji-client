9b9acde93c74a1d4d9af123db7e1041767e96050
ChaeYubin <cyoobin@naver.com>
2025-03-05 17:51:18 +0900

    fix: 출석 상세 옵션 수정 요청의 메서드 변경에 따른 파라미터 및 응답 수정

--- src/entities/attendance-option/api/mocks/modify-detail-option.ts
+++ src/entities/attendance-option/api/mocks/modify-detail-option.ts
@@ -1,10 +1,11 @@
 import { createMockHandler } from '@/shared/api'
 import { ATTENDANCE_OPTION_ENDPOINT } from '../endpoint'
 import { PutAttendanceOptionBodySchema } from '../modify-detail-option'
+import { AttendanceOptionKeySchema } from '../../model'
 import { OPTION_LIST_MOCK_DATA } from './option-list'
 
 export const modifyAttendanceDetailOptionMockHandler = createMockHandler({
-  endpoint: ATTENDANCE_OPTION_ENDPOINT.DETAIL(':detailOptionId'),
+  endpoint: ATTENDANCE_OPTION_ENDPOINT.DETAIL(':optionKey'),
   handler: async ({ request, params }) => {
     const body = await request.json()
     const { data } = PutAttendanceOptionBodySchema.safeParse(body)
@@ -13,20 +14,19 @@ export const modifyAttendanceDetailOptionMockHandler = createMockHandler({
       return { status: 400 }
     }
 
-    const { name } = data
-    const { detailOptionId } = params
-    const targetId = Number(detailOptionId)
+    const { data: optionKey } = AttendanceOptionKeySchema.safeParse(params.optionKey)
 
-    for (const option of Object.values(OPTION_LIST_MOCK_DATA)) {
-      const targetOption = option.detailOptions.find((opt) => opt.id === targetId)
-
-      if (targetOption) {
-        targetOption.name = name
-        return { data: {} }
-      }
+    if (!optionKey) {
+      return { status: 400 }
     }
 
-    return { status: 404 }
+    if (!OPTION_LIST_MOCK_DATA[optionKey]) {
+      return { status: 404 }
+    }
+
+    OPTION_LIST_MOCK_DATA[optionKey].detailOptions = data
+
+    return { data: {} }
   },
   method: 'put',
   delay: 1_200,
--- src/entities/attendance-option/api/modify-detail-option.ts
+++ src/entities/attendance-option/api/modify-detail-option.ts
@@ -1,15 +1,22 @@
 import { z } from 'zod'
 import { server } from '@/shared/api'
+import type { AttendanceOptionKey } from '../model'
 import { ATTENDANCE_OPTION_ENDPOINT } from './endpoint'
 
-export const PutAttendanceOptionBodySchema = z.object({
-  name: z.string(),
-})
+export const PutAttendanceOptionBodySchema = z.array(
+  z.object({
+    id: z.number(),
+    name: z.string(),
+  }),
+)
 
 export type PutAttendanceOptionsBody = z.infer<typeof PutAttendanceOptionBodySchema>
 
-export const modifyAttendanceDetailOption = (id: number, body: PutAttendanceOptionsBody) =>
-  server.request(ATTENDANCE_OPTION_ENDPOINT.DETAIL(id), {
+export const modifyAttendanceDetailOption = (
+  optionKey: AttendanceOptionKey,
+  body: PutAttendanceOptionsBody,
+) =>
+  server.request(ATTENDANCE_OPTION_ENDPOINT.DETAIL(optionKey), {
     schema: z.any(),
     body: JSON.stringify(body),
     method: 'PUT',
