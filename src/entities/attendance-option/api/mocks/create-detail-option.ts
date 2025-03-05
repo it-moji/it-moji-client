fda1d835dc4b7a4a70abe9d0416d4d3b3ed23ecb
ChaeYubin <cyoobin@naver.com>
2025-03-05 18:00:21 +0900

    refactor: 출석 상세 옵션 관련 mocking 핸들러 예외 발생 로직 추가

--- src/entities/attendance-option/api/mocks/create-detail-option.ts
+++ src/entities/attendance-option/api/mocks/create-detail-option.ts
@@ -1,38 +1,42 @@
 import { createMockHandler } from '@/shared/api'
 import { PostAttendanceOptionBodySchema } from '../create-detail-option'
 import { ATTENDANCE_OPTION_ENDPOINT } from '../endpoint'
-import type { AttendanceOptionKey } from '../../model'
 import { AttendanceOptionKeySchema } from '../../model'
 import { OPTION_LIST_MOCK_DATA } from './option-list'
 
-const generateCreateDetailOptionMockHandler = (optionKey: AttendanceOptionKey) =>
-  createMockHandler({
-    endpoint: ATTENDANCE_OPTION_ENDPOINT.DETAIL(optionKey),
-    handler: async ({ request }) => {
-      const body = await request.json()
-      const { data } = PostAttendanceOptionBodySchema.safeParse(body)
+export const createAttendanceDetailOptionMockHandler = createMockHandler({
+  endpoint: ATTENDANCE_OPTION_ENDPOINT.DETAIL(':optionKey'),
+  handler: async ({ request, params }) => {
+    const body = await request.json()
+    const { data } = PostAttendanceOptionBodySchema.safeParse(body)
 
-      if (!data) {
-        return { status: 400 }
-      }
+    if (!data) {
+      return { status: 400 }
+    }
 
-      const { name } = data
+    const { name } = data
 
-      const allDetailOptions = Object.values(OPTION_LIST_MOCK_DATA).flatMap(
-        (option) => option.detailOptions,
-      )
+    const { data: optionKey } = AttendanceOptionKeySchema.safeParse(params.optionKey)
 
-      const maxId =
-        allDetailOptions.length > 0 ? Math.max(...allDetailOptions.map((opt) => opt.id)) : 0
+    if (!optionKey) {
+      return { status: 400 }
+    }
 
-      OPTION_LIST_MOCK_DATA[optionKey]?.detailOptions.push({ id: maxId + 1, name })
+    if (!OPTION_LIST_MOCK_DATA[optionKey]) {
+      return { status: 404 }
+    }
 
-      return { data: {} }
-    },
-    method: 'post',
-    delay: 1_200,
-  })
+    const allDetailOptions = Object.values(OPTION_LIST_MOCK_DATA).flatMap(
+      (option) => option.detailOptions,
+    )
 
-export const createAttendanceDetailOptionMockHandlers = AttendanceOptionKeySchema.options.map(
-  (optionKey) => generateCreateDetailOptionMockHandler(optionKey),
-)
+    const maxId =
+      allDetailOptions.length > 0 ? Math.max(...allDetailOptions.map((opt) => opt.id)) : 0
+
+    OPTION_LIST_MOCK_DATA[optionKey]?.detailOptions.push({ id: maxId + 1, name })
+
+    return { data: {} }
+  },
+  method: 'post',
+  delay: 1_200,
+})
--- src/entities/attendance-option/api/mocks/option-detail.ts
+++ src/entities/attendance-option/api/mocks/option-detail.ts
@@ -1,21 +1,27 @@
 import type { GetAttendanceOptionsResponse } from '../get-option-detail'
 import { createMockHandler } from '@/shared/api'
 import { ATTENDANCE_OPTION_ENDPOINT } from '../endpoint'
-import type { AttendanceOptionKey } from '../../model'
 import { AttendanceOptionKeySchema } from '../../model'
 import { OPTION_LIST_MOCK_DATA } from './option-list'
 
-const generateAttendanceDetailOptionMockHandler = (optionKey: AttendanceOptionKey) =>
-  createMockHandler<GetAttendanceOptionsResponse['data']>({
-    endpoint: ATTENDANCE_OPTION_ENDPOINT.DETAIL(optionKey),
-    handler: () => {
-      return Promise.resolve({
-        data: OPTION_LIST_MOCK_DATA[optionKey]?.detailOptions || [],
-      })
-    },
-    delay: 300,
-  })
+export const attendanceOptionDetailMockHandler = createMockHandler<
+  GetAttendanceOptionsResponse['data']
+>({
+  endpoint: ATTENDANCE_OPTION_ENDPOINT.DETAIL(':optionKey'),
+  handler: ({ params }) => {
+    const { data: optionKey } = AttendanceOptionKeySchema.safeParse(params.optionKey)
 
-export const attendanceOptionDetailMockHandlers = AttendanceOptionKeySchema.options.map(
-  (optionKey) => generateAttendanceDetailOptionMockHandler(optionKey),
-)
+    if (!optionKey) {
+      return Promise.resolve({ data: null, status: 400 })
+    }
+
+    if (!OPTION_LIST_MOCK_DATA[optionKey]) {
+      return Promise.resolve({ data: null, status: 404 })
+    }
+
+    return Promise.resolve({
+      data: OPTION_LIST_MOCK_DATA[optionKey].detailOptions,
+    })
+  },
+  delay: 300,
+})
