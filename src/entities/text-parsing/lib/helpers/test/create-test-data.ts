c6ea699aacc9c030ea744d8a2df4c38b65c746d6
ChaeYubin <cyoobin@naver.com>
2025-03-27 21:02:17 +0900

    refactor: 텍스트 파싱 로직 테스트 데이터 생성 함수 수정

--- src/entities/text-parsing/lib/helpers/test/create-test-data.ts
+++ src/entities/text-parsing/lib/helpers/test/create-test-data.ts
@@ -6,46 +6,57 @@ interface TestDataParams {
   nameIdentifier?: ParsingOptions['name']
   personDelimiter?: ParsingOptions['delimiter']['person']
   titleDelimiter?: ParsingOptions['delimiter']['title']
+  lineDelimiter?: ParsingOptions['delimiter']['line']
   dayMapping?: ParsingOptions['dayMapping']
 }
 
 export const createTestData = ({
-  name = '이름',
+  name = '홍길동',
   nameIdentifier = PARSING_OPTIONS_MOCK_DATA.name,
   personDelimiter = PARSING_OPTIONS_MOCK_DATA.delimiter.person,
   titleDelimiter = PARSING_OPTIONS_MOCK_DATA.delimiter.title,
+  lineDelimiter = PARSING_OPTIONS_MOCK_DATA.delimiter.line,
   dayMapping = PARSING_OPTIONS_MOCK_DATA.dayMapping,
-}: TestDataParams) => `${name} ${personDelimiter} 2025-03-10 오전 9:29
-${nameIdentifier}${titleDelimiter} ${name}
-이번 주 한 마디: 아자아자 파이팅
-
-${dayMapping.monday}${titleDelimiter}🎖️ 
-TIL 내용
-${dayMapping.tuesday}${titleDelimiter}🎖️ 
-TIL 내용
-${dayMapping.wednesday}${titleDelimiter}🌱
-${dayMapping.thursday}${titleDelimiter}🌱
-TIL 내용
-${dayMapping.friday}${titleDelimiter}🌱
-휴식
-
-${dayMapping.saturday}${titleDelimiter}🌱
-TIL 내용
-${dayMapping.sunday}${titleDelimiter}🌱`
+}: TestDataParams) =>
+  name +
+  ' ' +
+  personDelimiter +
+  ' ' +
+  '2025-03-10 오전 9:29' +
+  lineDelimiter +
+  (nameIdentifier + titleDelimiter + name + lineDelimiter) +
+  ('이번 주 한 마디: 아자아자 파이팅' + lineDelimiter) +
+  lineDelimiter +
+  (dayMapping.monday + titleDelimiter + '🎖️' + lineDelimiter + 'TIL 내용' + lineDelimiter) +
+  (dayMapping.tuesday + titleDelimiter + '🎖️' + lineDelimiter + 'TIL 내용' + lineDelimiter) +
+  (dayMapping.wednesday + titleDelimiter + '🌱' + lineDelimiter) +
+  (dayMapping.thursday + titleDelimiter + '🌱' + lineDelimiter + 'TIL 내용' + lineDelimiter) +
+  (dayMapping.friday + titleDelimiter + '🌱' + lineDelimiter + '휴식' + lineDelimiter) +
+  lineDelimiter +
+  (dayMapping.saturday + titleDelimiter + '🌱' + lineDelimiter + 'TIL 내용' + lineDelimiter) +
+  (dayMapping.sunday + titleDelimiter + '🌱')
 
 interface TestDataListParams extends Omit<TestDataParams, 'name'> {
   nameList?: string[]
 }
 
 export const createTestDataList = ({
-  nameList = ['이름1', '이름2'],
+  nameList = ['홍길동', '김철수'],
   nameIdentifier = PARSING_OPTIONS_MOCK_DATA.name,
   personDelimiter = PARSING_OPTIONS_MOCK_DATA.delimiter.person,
   titleDelimiter = PARSING_OPTIONS_MOCK_DATA.delimiter.title,
+  lineDelimiter = PARSING_OPTIONS_MOCK_DATA.delimiter.line,
   dayMapping = PARSING_OPTIONS_MOCK_DATA.dayMapping,
 }: TestDataListParams) =>
   nameList
     .map((name) =>
-      createTestData({ name, personDelimiter, titleDelimiter, nameIdentifier, dayMapping }),
+      createTestData({
+        name,
+        personDelimiter,
+        titleDelimiter,
+        lineDelimiter,
+        nameIdentifier,
+        dayMapping,
+      }),
     )
     .join('')
