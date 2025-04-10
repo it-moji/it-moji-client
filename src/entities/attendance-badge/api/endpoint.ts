e3812cdf5423d66553ceb7c9f93d25b3330e6dab
ChanLee_KR <cl700120@gmail.com>
2025-04-10 21:12:59 +0900

    refactor: 사용하지 않는 revalidate 태그 삭제

--- src/entities/attendance-badge/api/endpoint.ts
+++ src/entities/attendance-badge/api/endpoint.ts
@@ -6,7 +6,3 @@ export const ATTENDANCE_BADGE_ENDPOINT = {
   DETAIL: (badgeId: AttendanceBadge['id'] | ':badgeId') =>
     `${ATTENDANCE_BADGE_ENDPOINT.LIST}/${badgeId}` as const,
 } as const
-
-export const ATTENDANCE_BADGE_TAG = {
-  BADGE_LIST_WITH_CONDITIONS: 'revalidate-badge-list-with-conditions-tag',
-} as const
--- src/entities/attendance-badge/api/get-badge-list-with-conditions.ts
+++ src/entities/attendance-badge/api/get-badge-list-with-conditions.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 import { type CommonResponse, server } from '@/shared/api'
-import { ATTENDANCE_BADGE_ENDPOINT, ATTENDANCE_BADGE_TAG } from './endpoint'
+import { ATTENDANCE_BADGE_ENDPOINT } from './endpoint'
 import { GetAttendanceBadgeDetailResponseSchema } from './get-badge-detail'
 
 export const GetAttendanceBadgeListWithConditionsResponseSchema = z.array(
@@ -18,7 +18,4 @@ export type GetAttendanceBadgeListWithConditionsResponse = CommonResponse<
 export const getAttendanceBadgeListWithConditions = () =>
   server.request(ATTENDANCE_BADGE_ENDPOINT.LIST_WITH_CONDITIONS, {
     schema: GetAttendanceBadgeListWithConditionsResponseSchema,
-    next: {
-      tags: [ATTENDANCE_BADGE_TAG.BADGE_LIST_WITH_CONDITIONS],
-    },
   })
