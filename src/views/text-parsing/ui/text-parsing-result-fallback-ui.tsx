5b1a3e6051ce326099d51f315a448d9c69f7922f
ChaeYubin <cyoobin@naver.com>
2025-04-20 10:49:05 +0900

    design: 텍스트 파싱 결과 폼 예외 대체 UI 디자인 변경

--- src/views/text-parsing/ui/text-parsing-result-fallback-ui.tsx
+++ src/views/text-parsing/ui/text-parsing-result-fallback-ui.tsx
@@ -1,4 +1,4 @@
-import { Text } from '@mantine/core'
+import { List } from '@mantine/core'
 import { DEFAULT_LINE_DELIMITER, TextParsingException } from '@/entities/text-parsing'
 import { cn } from '@/shared/lib'
 import { Icon } from '@/shared/ui'
@@ -11,34 +11,47 @@ export const TextParsingResultErrorFallback: React.FC<TextParsingResultErrorFall
   errorList,
 }) => {
   const lines = errorList[0].rawText.split(DEFAULT_LINE_DELIMITER)
-  const errorLineIndexList = errorList.flatMap((error) => error.errorLineIndexArray)
+  const errorLineIndexList = new Set(errorList.flatMap((error) => error.errorLineIndexArray ?? []))
 
   return (
-    <div className="flex flex-col items-center gap-3 pt-3">
-      <Icon query="fluent:warning-28-regular" className="size-20" />
-      <div className="space-y-0 text-center">
-        {errorList.map((error) => (
-          <Text key={TextParsingException.extractMessage(error)} className="font-medium">
-            {error.errorLineNumberArray && `[${error.errorLineNumberArray?.join(', ')}행] `}
-            {TextParsingException.extractMessage(error)}
-          </Text>
-        ))}
+    <div className="space-y-3 py-1">
+      <div className="flex space-x-2">
+        <Icon query="fluent:warning-28-regular" className="mt-0.5 size-5" />
+        <p className="break-keep font-bold">아래의 이유로 텍스트 파싱에 실패했어요</p>
       </div>
-      <div className="mt-2 max-h-56 w-full overflow-auto rounded-md border border-solid border-red-300 bg-red-100 py-2 text-sm dark:border-rose-200/20 dark:bg-rose-300/20">
+      <List
+        className="space-y-0 pl-3"
+        icon={
+          <span className="block size-1.5 rounded-full bg-[var(--mantine-color-placeholder)]" />
+        }
+        component="ul"
+      >
+        {errorList.map((error) => (
+          <List.Item
+            key={TextParsingException.extractMessage(error)}
+            className="break-keep font-medium"
+            component="li"
+          >
+            {TextParsingException.extractMessage(error)}
+            {error.errorLineNumberArray && ` (${error.errorLineNumberArray.join(', ')}행)`}
+          </List.Item>
+        ))}
+      </List>
+      <pre className="mt-2 max-h-56 w-full overflow-auto rounded-md border border-solid border-red-300 bg-[var(--mantine-color-red-light)] py-2 text-sm dark:border-red-900">
         {lines.map((line, idx) => (
-          <div
+          <span
             key={idx}
             className={cn('flex pl-3', {
-              'bg-red-300 dark:bg-rose-600/30': errorLineIndexList.includes(idx),
+              'bg-[var(--mantine-color-red-light-hover)]': errorLineIndexList.has(idx),
             })}
           >
-            <div className="w-6 flex-none pr-2 text-right font-mono text-dark-300 dark:text-dark-200">
+            <span className="w-6 flex-none pr-2 text-right font-mono text-dark-300 dark:text-dark-200">
               {idx + 1}
-            </div>
+            </span>
             <span className="text-dark-900 dark:text-dark-50">{line}</span>
-          </div>
+          </span>
         ))}
-      </div>
+      </pre>
     </div>
   )
 }
