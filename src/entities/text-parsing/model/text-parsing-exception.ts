bcaecf9c11615960a181bc2f3164da63cfcd9859
ChaeYubin <cyoobin@naver.com>
2025-04-20 11:17:40 +0900

    refactor: 텍스트 파싱 예외 처리 클래스 구조 간소화

--- src/entities/text-parsing/model/text-parsing-exception.ts
+++ src/entities/text-parsing/model/text-parsing-exception.ts
@@ -1,42 +1,15 @@
 import { Exception } from '@/shared/api'
 
 export class TextParsingException extends Exception {
-  private _rawText: string
-  private _errorLineIndexArray?: number[]
-  private _errorLineNumberArray?: number[]
+  public rawText: string
+  public errorLineIndexArray?: number[]
 
   public constructor(cause: string, rawText: string, errorLineIndexArray?: number[]) {
     super(cause)
 
     this.name = this.constructor.name
 
-    this._rawText = rawText
-    this._errorLineIndexArray = errorLineIndexArray
-    this._errorLineNumberArray = errorLineIndexArray?.map((lineIndex) => lineIndex + 1)
-  }
-
-  public get rawText() {
-    return this._rawText
-  }
-
-  public set rawText(rawText: string) {
-    this._rawText = rawText
-  }
-
-  public get errorLineIndexArray() {
-    return this._errorLineIndexArray
-  }
-
-  public set errorLineIndexArray(errorLineIndexArray: number[] | undefined) {
-    this._errorLineIndexArray = errorLineIndexArray
-    this._errorLineNumberArray = errorLineIndexArray?.map((lineIndex) => lineIndex + 1)
-  }
-
-  public get errorLineNumberArray() {
-    return this._errorLineNumberArray
-  }
-
-  public set errorLineNumberArray(errorLineNumberArray: number[] | undefined) {
-    this._errorLineNumberArray = errorLineNumberArray
+    this.rawText = rawText
+    this.errorLineIndexArray = errorLineIndexArray
   }
 }
