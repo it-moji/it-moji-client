a22edf8fd5a2512c5b3851a3aafca02f9d9389e4
ChaeYubin <cyoobin@naver.com>
2025-04-27 15:15:32 +0900

    remove: 사용하지 않는 코드 제거

--- src/widgets/attendance-options-select/config/attendance-option-select-bg-color.ts
+++
@@ -1,9 +0,0 @@
-import { AttendanceOptionKeySchema } from '@/entities/attendance-option'
-
-export const ATTENDANCE_OPTION_SELECT_BG_COLOR = {
-  [AttendanceOptionKeySchema.Enum.attendance]: 'bg-green-50',
-  [AttendanceOptionKeySchema.Enum.absence]: 'bg-red-50',
-  [AttendanceOptionKeySchema.Enum.vacation]: 'bg-blue-50',
-  [AttendanceOptionKeySchema.Enum.gap]: 'bg-yellow-50',
-  [AttendanceOptionKeySchema.Enum.rest]: '',
-} as const
--- src/widgets/attendance-options-select/config/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './attendance-option-select-bg-color'
